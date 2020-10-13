bce29db07dad1051ecbf31a13aadcbec27cdcda7
Leonardo Sandes Rinaldi <leo.sr.2010_live@hotmail.com>
2020-10-12 23:02:31 -0300

    bug fixed

--- VSSourceControl/Program.cs
+++ VSSourceControl/Program.cs
@@ -13,7 +13,8 @@ namespace VSSourceControl
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
-            Console.WriteLine("from second commit at master branch!");
+            Console.WriteLine("from third commit at master branch!");
+            Console.WriteLine("bug fixed! 🦟🦟🦟");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
